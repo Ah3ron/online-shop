a16499755e0c8275c8f79e55ea93e0ae785c2e87
Ah3ron <ah3ron@protonmail.com>
2024-10-02 23:23:49 +0300

    Fix api request

--- frontend/src/routes/products/+page.svelte
+++ frontend/src/routes/products/+page.svelte
@@ -7,7 +7,7 @@
 
   async function fetchProducts() {
     try {
-      const response = await fetch(import.meta.env.API_URL);
+      const response = await fetch(import.meta.env.API_URL + "api/products");
       if (!response.ok) {
         throw new Error("Ошибка при загрузке данных");
       }
